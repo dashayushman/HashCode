96eefa7b95bd85e6ed400c9c4ae9e7b87b7b021f
dashayushman <dash.ayushman.99@gmail.com>
2016-02-11 21:53:03 +0100

    baba added

--- src/com/hashcode/bolbhum/modules/Baba.java
+++ src/com/hashcode/bolbhum/modules/Baba.java
@@ -2,17 +2,18 @@ package com.hashcode.bolbhum.modules;
 
 import java.util.ArrayList;
 
+import com.hashcode.bolbhum.beans.Drone;
+import com.hashcode.bolbhum.beans.Order;
+import com.hashcode.bolbhum.beans.Params;
 import com.hashcode.bolbhum.utility.Utils;
 
 public class Baba {
 	public static void main(String[] args) {
 		try {
-			String filecontent = Utils.getInstance().getStringFromFile("C:\\Users\\Amit\\Desktop\\hash code\\learn_and_teach.in");
-			System.out.println(filecontent);
-			ArrayList<String> lines = Utils.getInstance().getLineStringsFromFile("C:\\Users\\Amit\\Desktop\\hash code\\learn_and_teach.in");
-			for (String string : lines) {
-				System.out.println(string);
-			}
+			Utils utils = Utils.getInstance();
+			Params params = utils.getParameters();
+			ArrayList<Order> ordersList = params.getOrdersList();
+
 		} catch (Exception e) {
 			e.printStackTrace();
 			String message = e.getMessage();
@@ -25,5 +26,76 @@ public class Baba {
 				break;
 			}
 		}
+	}
+
+	public static Drone getDrone(Order tempOrder) {
+		Utils utils = Utils.getInstance();
+		Params params = utils.getParameters();
+		ArrayList<Order> ordersList = params.getOrdersList();
+		int bestOrder;
+		int bestOrderWeight = 1000000;
+		int orderWeight = 10000;
+		Order tempOrder1;
+		Drone tempDrone;
+		int pos1i, pos1j, pos2i, pos2j;
+
+		ArrayList<Drone> droneList = params.getDroneList();
+		for (int i = 0; i < droneList.size(); i++) {
+			tempDrone = droneList.get(i);
+			pos1i = tempOrder1.getLocation().getRow();
+			pos1j = tempOrder1.getLocation().getColumn();
+
+			pos2i = tempDrone.getLocation().getRow();
+			pos2j = tempDrone.getLocation().getColumn();
+
+			orderWeight = euclidDistance(pos1i, pos1j, pos2i, pos2j)
+					+ tempDrone.getEnd();
+			if (orderWeight < bestOrderWeight) {
+				bestOrderWeight = orderWeight;
+				bestOrder = i;
+			}
+		}
+		droneList.get(bestOrder).setStart(droneList.get(bestOrder).getEnd());
+		return droneList.get(bestOrder);
+	}
+
+	public static int euclidDistance(int pos1i, int pos1j, int pos2i, int pos2j) {
+		double temp = Math.pow(pos1i - pos2i, 2) + Math.pow(pos1j - pos2j, 2);
+		temp = Math.sqrt(temp);
+		temp = Math.ceil(temp);
+		return (int) temp;
+	}
+
+	public static Order getOrder() {
+		Utils utils = Utils.getInstance();
+		Params params = utils.getParameters();
+		ArrayList<Order> ordersList = params.getOrdersList();
+		int bestOrder;
+		int bestOrderWeight = 1000000;
+		int orderWeight = 10000;
+		Order tempOrder;
+		Drone tempDrone;
+		int pos1i, pos1j, pos2i, pos2j;
+
+		ArrayList<Drone> droneList = params.getDroneList();
+		for (int i = 0; i < ordersList.size(); i++) {
+			for (int j = 0; j < droneList.size(); j++) {
+				tempOrder = ordersList.get(i);
+				tempDrone = droneList.get(j);
+				pos1i = tempOrder.getLocation().getRow();
+				pos1j = tempOrder.getLocation().getColumn();
+
+				pos2i = tempDrone.getLocation().getRow();
+				pos2j = tempDrone.getLocation().getColumn();
+
+				orderWeight = euclidDistance(pos1i, pos1j, pos2i, pos2j)
+						+ tempDrone.getEnd();
+				if (orderWeight < bestOrderWeight) {
+					bestOrderWeight = orderWeight;
+					bestOrder = i;
+				}
+			}
+		}
+		return ordersList.get(bestOrder);
 	}
 }
